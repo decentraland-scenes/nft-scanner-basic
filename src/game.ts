aa860aa61b7359d3c5edd02620e726b8e23ecdc9
digimindmarket <digimindmarket@gmail.com>
2022-04-13 13:51:21 +0300

    no change

--- src/game.ts
+++ src/game.ts
@@ -4,14 +4,18 @@ import * as crypto from "@dcl/crypto-scene-utils"
 import { Door } from "./door"
 import { Sound } from "./sound"
 import * as ui from "@dcl/ui-scene-utils"
+import * as Players from "@decentraland/Players"
+import { getUserData, UserData } from '@decentraland/Identity'
 
 // Config
 let userAddress: string
+export let userData: UserData
 
 // Example token from the contract: https://opensea.io/assets/0x6b47e7066c7db71aa04a1d5872496fe05c4c331f/2
 // Contract address on Etherscan: https://etherscan.io/address/0x6b47e7066c7db71aa04a1d5872496fe05c4c331f
 //const contractAddress = "0x5CC5B05a8A13E3fBDB0BB9FcCd98D38e50F90c38" // SANDBOX LAND
-const contractAddress = "0x4da3978b45A3A7d931c93F543D2492bD1ba675BF" // SAMSUNG 837X - 8 
+//const contractAddress = "0x4da3978b45A3A7d931c93F543D2492bD1ba675BF" // SAMSUNG 837X - 8 
+const contractAddress = "0x3845badAde8e6dFF049820680d1F14bD3903a5d0" // SAND 
 
 // Sounds
 const openDoorSound = new Sound(new AudioClip("sounds/openDoor.mp3"), false)
@@ -34,21 +38,35 @@ facade.addComponent(new Transform({ position: new Vector3(8, 0.05, 10) }))
 facade.getComponent(Transform).rotate(Vector3.Up(), 180)
 engine.addEntity(facade)
 
+
+
 // Door
 const door = new Door(new GLTFShape("models/door.glb"))
 door.setParent(facade)
 door.addComponent(
   new OnPointerDown(
     () => {
+      
       checkTokens()
     },
     {
       button: ActionButton.PRIMARY,
-      hoverText: "Enter Club",
+      hoverText: "Enter club",
       showFeedback: true,
     }
   )
 )
+
+// On load
+executeTask(async () => {
+  try {
+    userAddress = await EthereumController.getUserAccount()
+    //userAddress = (await userData).publicKey
+    log("User Address: ", userAddress)
+  } catch (error) {
+    log("PUPUPU")
+  }
+})
 
 // UI
 let noSign = new ui.CenterImage("images/no-sign.png", 1, true, 0, 20, 128, 128, {
@@ -58,18 +76,11 @@ let noSign = new ui.CenterImage("images/no-sign.png", 1, true, 0, 20, 128, 128, 
   sourceTop: 0,
 })
 
-// On load
-executeTask(async () => {
-  try {
-    userAddress = await EthereumController.getUserAccount()
-    log("User Address: ", userAddress)
-  } catch (error) {
-    log(error.toString())
-  }
-})
+
 
 // Check player's wallet to see if they're holding any tokens relating to that contract address
 async function checkTokens() {
+  
   let balance = await crypto.currency.balance(contractAddress, userAddress)
   log("BALANCE: ", balance)
 
